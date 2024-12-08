0058451765c6133453c366f9b1c600847920ec83
Strokin Constantine <strokinconstantine@gmail.com>
2024-12-08 15:55:03 +0300

    pet_projects/zhirkov_low_level_programming_assignments/image_rotation fix: removal of redundant directories

--- pet_projects/zhirkov_low_level_programming_assignments/image_rotation/src/constants.c
+++
@@ -1,7 +0,0 @@
-#include "constants.h"
-#include <stdint.h>
-
-const int_fast8_t PIXEL_SIZE_IN_BYTES = 3;
-const double CONSTANT_ROTATION_ANGLE_IN_DEGREES = 90.0;
-const double ROTATION_ANGLE_PRECISION_IN_DEGREES = 0.01;
-
